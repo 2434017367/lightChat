d1c2ab087d9214598d0e17f662c2681680d2170b
zhy <39161510+2434017367@users.noreply.github.com>
2019-06-15 15:05:18 +0800

    初始化版本

---
+++ src/main/java/top/zenghy/lightchat/LightchatApplication.java
@@ -0,0 +1,13 @@
+package top.zenghy.lightchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightchatApplication.class, args);
+    }
+
+}
